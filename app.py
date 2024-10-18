d0dc9b5531573672c8c1453d38f60c10d9adacc4
PremKumarHub <ey78627@gmail.com>
2024-10-18 20:24:15 +0530

    Add styles for Add Farmer page and improve responsiveness

--- app.py
+++ app.py
@@ -19,22 +19,20 @@ def index():
 
 @app.route('/add_farmer', methods=['GET', 'POST'])
 def add_farmer():
+    conn = get_db_connection()
     if request.method == 'POST':
-        farmer_id = request.form['farmer_id']
         name = request.form['name']
+        farmer_id = request.form['farmer_id']
+
         if name and farmer_id:
-            try:
-                conn = get_db_connection()
-                conn.execute('INSERT INTO farmers (id, name) VALUES (?, ?)', (farmer_id, name))
-                conn.commit()
-                conn.close()
-                flash('Farmer added successfully!')
-                return redirect(url_for('index'))
-            except sqlite3.IntegrityError:
-                flash('Farmer ID must be unique!')
-        else:
-            flash('Farmer name and ID cannot be empty!')
-    return render_template('add_farmer.html')
+            conn.execute('INSERT INTO farmers (id, name) VALUES (?, ?)', (farmer_id, name))
+            conn.commit()
+            return redirect(url_for('add_farmer'))  # Redirect to the same page to refresh the table
+
+    farmers = conn.execute('SELECT * FROM farmers').fetchall()
+    conn.close()
+    return render_template('add_farmer.html', farmers=farmers)
+
 
 @app.route('/log_transaction', methods=['GET', 'POST'])
 def log_transaction():
@@ -63,6 +61,13 @@ def transactions():
     trans = conn.execute('SELECT f.id as farmer_id, f.name, t.amount, t.description, t.date FROM transactions t JOIN farmers f ON t.farmer_id = f.id').fetchall()
     conn.close()
     return render_template('transactions.html', transactions=trans)
+@app.route('/show_farmers')
+def show_farmers():
+    conn = get_db_connection()
+    farmers = conn.execute('SELECT * FROM farmers').fetchall()
+    conn.close()
+    return render_template('show_farmers.html', farmers=farmers)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
